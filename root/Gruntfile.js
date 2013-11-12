21bf1e1a9ea4767a9cd0c55b41fba8cf86320b83
Greg Allen <me@jga.me>
2013-11-12 15:44:44 -0800

    use load-grunt-config and shell task to checkout file from master. fixes #2 fixes #1

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,38 +1,11 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    doc: 'index.md',
-    watch: {
-      md: {
-        files: '<%= doc %>',
-        tasks: 'default'
-      }
-    },
-    connect: {
-      server: {
-        port: 8000,
-        base: '.'
-      }
-    },
-    shell: {
-      build: {
-        command: 'ghpage <%= doc %>',
-        options: {
-          stdout: true
-        }
-      }
-    },
-    reloadr: {
-      main: [
-        'index.html'
-      ]
+
+
+  require('load-grunt-config')(grunt, {
+    config: {
+      masterDoc: 'README.md'
+      doc: 'README.md'
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-reloadr');
-
-  grunt.registerTask('default', ['shell:build']);
-  grunt.registerTask('dev', ['connect:server', 'reloadr', 'watch']);
 };
