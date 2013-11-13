f157865e971ccdcd2435a76473e0315de431111f
Greg Allen <me@jga.me>
2013-11-12 16:33:19 -0800

    tweaks

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -3,8 +3,16 @@ module.exports = function(grunt) {
 
   require('load-grunt-config')(grunt, {
     config: {
-      masterDoc: 'README.md'
-      doc: 'README.md'
+      masterDoc: 'README.md',
+      doc: 'README.md',
+      design: 'simple'
+      data: {
+        pageTitle: '',
+        analytics: 'UA-24017782-2',
+        github: 'github.com/',
+        builtBy: 'First + Third',
+        twitterShare: ''
+      }
     }
   });
 
